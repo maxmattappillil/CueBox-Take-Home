906ad9cd65a1e0c828bc83a691749268ea45dcb6
maxmattappillil <max.mattappillil@gmail.com>
2024-12-11 12:44:33 -0500

    refactor: Organize FastAPI project structure with routers and update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
+from app.routers import hello
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(hello.router)
