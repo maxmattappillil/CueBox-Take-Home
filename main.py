fc7428fc23d45ef84497839f8793da3ccc660f6e
maxmattappillil <max.mattappillil@gmail.com>
2024-12-11 12:50:51 -0500

    feat: Add favicon.ico and serve static files in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 from app.routers import hello
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(hello.router)
