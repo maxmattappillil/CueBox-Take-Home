aa993a89f8312f45f84ad43b4747c7f7cc56058a
maxmattappillil <max.mattappillil@gmail.com>
2024-12-11 14:11:23 -0500

    refactor: Rename hello.py to file_upload.py for clarity on functionality

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
-from app.routers import hello
+from app.routers import file_upload
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-app.include_router(hello.router)
+app.include_router(file_upload.router)
